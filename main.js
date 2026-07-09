69d4dd92cb81228fbf48fb475cc7c05142881a04
MaskWhite1 <samuel.juca@escola.pr.gov.br>
2026-07-09 13:02:57 +0000

    .

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const numeroSenha = document.querySelector('.parametro-senha__texto');
 numeroSenha.textContent = 5;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 botoes[0].onclick = diminuiTamanho;
+
 console.log(botoes);
 function diminuiTamanho(){ 
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha
 }
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
